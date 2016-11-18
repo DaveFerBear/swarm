c74aac67db16bbfe4d0da6c77f30dc6fb799d3e0
David Ferris <david.j.ferris.21@gmail.com>
2016-11-17 23:47:24 -0500

    org class

--- js/rendezvous.js
+++ js/rendezvous.js
@@ -1,13 +1,23 @@
 function test() {
+
+    class Org {
+        constructor(_height, _width) {
+            this.height = _height;
+            this.width = _width;
+        }
+    }
+
     var canvas = d3.select('body')
             .append('svg')
             .attr('width', screen.width)
             .attr('height', screen.height);
 
     for (var x = 0; x < 100; x++) {
-        var circle = canvas.append('circle')
-            .attr('cx', Math.random()*screen.width)
-            .attr('cy', Math.random()*screen.height)
+        var circle = new Org(Math.random()*screen.width, Math.random()*screen.height);
+
+        canvas.append('circle')
+            .attr('cx', circle.width)
+            .attr('cy', circle.height)
             .attr('r', 5)
             .attr('fill', 'pink');
     }
