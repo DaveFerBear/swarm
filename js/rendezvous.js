cdc7e70152d0ebd2e327893c543419127cc6dbc1
David Ferris <david.j.ferris.21@gmail.com>
2016-11-18 00:23:36 -0500

    identifying object and animating

--- js/rendezvous.js
+++ js/rendezvous.js
@@ -1,12 +1,12 @@
 function test() {
 
     class Org {
-        constructor(_height, _width) {
-            this.height = _height;
+        constructor(_width, _height) {
             this.width = _width;
+            this.height = _height;
         }
     }
-
+    var orgs = [];
     var canvas = d3.select('body')
             .append('svg')
             .attr('width', screen.width)
@@ -14,11 +14,17 @@ function test() {
 
     for (var x = 0; x < 100; x++) {
         var circle = new Org(Math.random()*screen.width, Math.random()*screen.height);
-
+        orgs.push(circle);
         canvas.append('circle')
+            .attr('id', Org.name + x)
             .attr('cx', circle.width)
             .attr('cy', circle.height)
             .attr('r', 5)
             .attr('fill', 'pink');
+        console.log(Org.name + x);
+    }
+
+    for (var b = 0; b < orgs.length; b++) {
+        d3.select('#' + Org.name + b).transition().attr('cx', 0).duration(1000);
     }
 }
