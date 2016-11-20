a7707a08dffd74d877c6537ff71ae4c50c07d7b3
David Ferris <david.j.ferris.21@gmail.com>
2016-11-19 21:29:14 -0500

    pink and FPS meter

--- js/rendezvous.js
+++ js/rendezvous.js
@@ -1,29 +1,39 @@
-
-
-	class Org {
-		constructor(_width, _height) {
-			this.width = _width;
-			this.height = _height;
-		}
+class Org {
+	constructor(_width, _height) {
+		this.width = _width;
+		this.height = _height;
 	}
+}
 
-	var orgs = d3.range(10).map(function() {
-		return new Org(Math.random() * screen.width, Math.random() * screen.height);
-	});
+var orgs = d3.range(100).map(function() {
+	return new Org(Math.random() * screen.width, Math.random() * screen.height);
+});
 
-	var canvas = d3.select('body')
-			.append('svg')
-			.attr('width', screen.width)
-			.attr('height', screen.height);
+var canvas = d3.select('body')
+		.append('svg')
+		.attr('width', screen.width)
+		.attr('height', screen.height);
 
-	var circles = canvas.selectAll("circle")
-			.data(orgs)
-			.enter().append('circle')
-			.attr('cx', d => d.width )
-			.attr('cy', d => d.height )
-			.attr('r', 5)
-			.attr('fill', 'pink');
+var circles = canvas.selectAll("circle")
+		.data(orgs)
+		.enter().append('circle')
+		.attr('cx', d => d.width )
+		.attr('cy', d => d.height )
+		.attr('r', 5)
+		.attr('fill', 'rgb(255, 0, 213)');
 
-	circles.transition().attr('cx', 0);
+var text = canvas.append("svg:text")
+        .attr("x", screen.width - 100)
+        .attr("y", 20);
+    
+var start = Date.now(),
+	frames = 0;
 
-	console.log('eg');
+d3.timer(function() {
+	//Update FPS
+	var now = Date.now(), duration = now - start;
+	text.text(~~(++frames * 1000 / duration));
+	if (duration >= 1000) frames = 0, start = now;
+});
+
+circles.transition().attr('cx', 0);
