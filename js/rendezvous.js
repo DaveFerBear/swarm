831649cb6fc5d1d7d05f2d11ac19d9d86d72c05f
David Ferris <david.j.ferris.21@gmail.com>
2016-11-19 19:25:59 -0500

    refactor in D3 way

--- js/rendezvous.js
+++ js/rendezvous.js
@@ -1,30 +1,28 @@
 function test() {
 
-    class Org {
-        constructor(_width, _height) {
-            this.width = _width;
-            this.height = _height;
-        }
-    }
-    var orgs = [];
-    var canvas = d3.select('body')
-            .append('svg')
-            .attr('width', screen.width)
-            .attr('height', screen.height);
+	class Org {
+		constructor(_width, _height) {
+			this.width = _width;
+			this.height = _height;
+		}
+	}
 
-    for (var x = 0; x < 100; x++) {
-        var circle = new Org(Math.random()*screen.width, Math.random()*screen.height);
-        orgs.push(circle);
-        canvas.append('circle')
-            .attr('id', Org.name + x)
-            .attr('cx', circle.width)
-            .attr('cy', circle.height)
-            .attr('r', 5)
-            .attr('fill', 'pink');
-        console.log(Org.name + x);
-    }
+	var orgs = d3.range(10).map(function() {
+		return new Org(Math.random() * screen.width, Math.random() * screen.height);
+	});
 
-    for (var b = 0; b < orgs.length; b++) {
-        d3.select('#' + Org.name + b).transition().attr('cx', 0).duration(1000);
-    }
+	var canvas = d3.select('body')
+			.append('svg')
+			.attr('width', screen.width)
+			.attr('height', screen.height);
+
+	var circles = canvas.selectAll("circle")
+			.data(orgs)
+			.enter().append('circle')
+			.attr('cx', d => d.width )
+			.attr('cy', d => d.height )
+			.attr('r', 5)
+			.attr('fill', 'pink');
+
+	circles.transition().attr('cx', 0);
 }
