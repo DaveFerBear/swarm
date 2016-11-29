d4e38b47443334ec939fff473e7719b5f43b98e5
David Ferris <david.j.ferris.21@gmail.com>
2016-11-28 21:20:21 -0500

    building using D3 architecture

--- js/rendezvous.js
+++ js/rendezvous.js
@@ -4,8 +4,6 @@ function test() {
 	}, 0);
 }
 
-var numBodies = 200;
-
 var canvas = d3.select('body')
 		.append('svg')
 		.attr('width', screen.width)
@@ -18,32 +16,53 @@ var text = canvas.append("svg:text")
 var start = Date.now(),
 	frames = 0;
 
+var data = d3.range(200).map(function() {
+	return {
+		x: 0,
+		y: 0,
+		vx: 0.001,
+		vy: 0.001
+	}
+});
+
+var x = d3.scaleLinear()
+    .domain([-5, 5])
+    .range([0, screen.width]);
+
+var y = d3.scaleLinear()
+    .domain([-5, 5])
+    .range([0, screen.height]);
+
+
+var circles = canvas.selectAll("circle")
+		.data(data)
+		.enter().append('circle')
+		.attr('cx', function(d) {
+			return d.x;
+		})
+		.attr('cy', function(d) {
+			return d.y;
+		})
+		.attr('r', 5)
+		.attr('fill', 'rgb(255, 0, 213)');
+
 d3.timer(function() {
 	//Update FPS
 	var now = Date.now(), duration = now - start;
 	text.text(~~(++frames * 1000 / duration));
 	if (duration >= 1000) frames = 0, start = now;
+
+	data.forEach(function(d) {
+		d.x += d.vx;
+		d.y += d.vy;
+	})
+
+	circles.attr("transform", function(d) {
+			return "translate(" + x(d.x) + "," + y(d.y) + ")";
+		}).attr("r", function(d) {
+			return Math.min(1 + 1000 * Math.abs(d.vx * d.vy), 10);
+	});
 });
-
-var orgs = [];
-
-for (var x = 0; x < numBodies; x++) {
-	orgs.push([Math.random() * screen.width, Math.random() * screen.height]);
-}
-
-var circles = canvas.selectAll("circle")
-		.data(orgs)
-		.enter().append('circle')
-		.attr('cx', function(d, i) {
-			d = orgs[i][0];
-			return d;
-		})
-		.attr('cy', function(d, i) {
-			d = orgs[i][1];
-			return d;
-		})
-		.attr('r', 5)
-		.attr('fill', 'rgb(255, 0, 213)');
 
 function update() {
 	for (var i = 0; i < numBodies; i++) {
